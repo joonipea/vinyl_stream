5be7e5e53ee3edebdc5262f11116922afc21d243
June Pratt <jpratt@ebgsolutions.com>
2025-12-02 17:45:51 -0500

    add mp3 info

--- tools/identifySong.ts
+++ tools/identifySong.ts
@@ -2,7 +2,7 @@ import fs from "node:fs"
 import { $ } from "bun";
 
 async function identifySong() {
-    console.log(await $`ffmpeg -sseof -6 -y -i ../Music/stream.flac ../Music/stream/last_six_seconds.mp3`.text())
+    console.log(await $`ffmpeg -sseof -6 -y -i ../Music/stream.flac -ab 320k -id3v2_version 3 ../Music/stream/last_six_seconds.mp3`.text())
     const fs_file = fs.createReadStream('../Music/stream/last_six_seconds.mp3');
     const bun_file = await Bun.file('../Music/stream/last_six_seconds.mp3').arrayBuffer();
     const songData = {
