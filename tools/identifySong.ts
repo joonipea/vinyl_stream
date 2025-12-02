3195d8f4d1fd5c4fa295eb7f51cf0815b86a03d8
June Pratt <juneprattm@gmail.com>
2025-12-02 18:47:27 -0500

    split commands

--- tools/identifySong.ts
+++ tools/identifySong.ts
@@ -2,7 +2,9 @@ import fs from "node:fs"
 import { $ } from "bun";
 
 async function identifySong() {
-    await $`yes | cp ../Music/stream.flac ../Music/stream_temp.flac  && ffmpeg -sseof -6 -y -i ../Music/stream_temp.flac -ab 320k -id3v2_version 3 ../Music/stream/last_six_seconds.mp3 && rm ../Music/stream_temp.flac`
+    await $`yes | cp ../Music/stream.flac ../Music/stream_temp.flac`
+    await $`ffmpeg -sseof -6 -y -i ../Music/stream_temp.flac -ab 320k -id3v2_version 3 ../Music/stream/last_six_seconds.mp3`
+
     const fs_file = fs.createReadStream('../Music/stream/last_six_seconds.mp3');
     const bun_file = await Bun.file('../Music/stream/last_six_seconds.mp3').arrayBuffer();
     const songData = JSON.stringify({
