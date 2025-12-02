897cf427975dbc8239618acfc14d3ee241869c7a
June Pratt <juneprattm@gmail.com>
2025-12-02 18:38:58 -0500

    copy stream to temp

--- tools/identifySong.ts
+++ tools/identifySong.ts
@@ -2,21 +2,25 @@ import fs from "node:fs"
 import { $ } from "bun";
 
 async function identifySong() {
-    await $`ffmpeg -sseof -6 -y -i ../Music/stream/last_six_seconds.mp3 -ab 320k -id3v2_version 3 ../Music/stream/last_six_secondsi.mp3`
+    await $`yes | cp ../Music/stream.flac ../Music/stream_temp.flac  && ffmpeg -sseof -6 -y -i ../Music/stream_temp.flac -ab 320k -id3v2_version 3 ../Music/stream/last_six_seconds.mp3 && rm ../Music/stream_temp.flac`
     const fs_file = fs.createReadStream('../Music/stream/last_six_seconds.mp3');
     const bun_file = await Bun.file('../Music/stream/last_six_seconds.mp3').arrayBuffer();
-    const songData = {
+    const songData = JSON.stringify({
         'api_token': Bun.env.AUDD_API_KEY,
         'file': fs_file,
         'return': 'musicbrainz,apple_music,spotify',
-    };
+    });
     const req = new Request({
         url: 'https://api.audd.io/',
         method: 'POST',
         body: songData,
         headers: { 'Content-Type': 'multipart/form-data' },
       });
-    const res = await fetch(req);
+    const res = await fetch('https://api.audd.io/',{
+        method: 'POST',
+        body: songData,
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
     console.log(res.ok, res.status);
     const res_json = await res.json()
     console.log(JSON.stringify(res_json));
