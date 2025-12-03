e594e62dcc97b7c70241755589824e78699a0fce
June Pratt <juneprattm@gmail.com>
2025-12-02 21:44:26 -0500

    override fingerprint

--- tools/identifySong.ts
+++ tools/identifySong.ts
@@ -4,11 +4,11 @@ async function identifySong() {
     try {
     await $`ffmpeg -n -i ../Music/stream/stream.m3u8 -segment_wrap 1 -segment_time 6 -t 6 ../Music/stream/last_six_seconds_current.mp3`
     } catch (e) {
-	console.log(e.message);
+	    console.log(e.message);
     }
 
     await $`yes | mv ../Music/stream/last_six_seconds_current.mp3 ../Music/stream/last_six_seconds.mp3`
-    await $`ffmpeg -i ../Music/stream/last_six_seconds.mp3 -f chromaprint ../Music/stream/current_fingerprint.txt`
+    await $`ffmpeg -y -i ../Music/stream/last_six_seconds.mp3 -f chromaprint ../Music/stream/current_fingerprint.txt`
     const current_fingerprint = await Bun.file("../Music/stream/current_fingerprint.txt").text();
     const acoustid_url = `https://api.acoustid.org/v2/lookup?client=${Bun.env.ACOUSTID_CLIENT_KEY}&duration=6&fingerprint=${current_fingerprint}`
     const res = await fetch(acoustid_url);
